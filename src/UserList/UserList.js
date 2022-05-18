c48ba7b01528c1a4d6889bc8fa4c6fce53e91e7a
Orpaz <orpaz234@gmail.com>
2022-05-18 18:17:19 +0300

    Navigate

--- src/UserList/UserList.js
+++ src/UserList/UserList.js
@@ -2,7 +2,7 @@ import person1 from '../Contacts/person1.jpeg'
 import person2 from '../Contacts/person2.jpeg'
 import person3 from '../Contacts/person3.jpeg'
 
-const UserList = [{ name:"userID", nickname:'orpi' , password:'orpaz12345', isAcct: false, img: person1},
+const UserList = [{ name:"userID123", nickname:'orpi' , password:'orpaz12345', isAcct: false, img: person1},
 { name:"hilalosh", nickname:"hila" , password:'hila12345' ,isAcct: false, img: person2},
 { name:"hemihemi",nickname: "hemi", password:'hemi12345' ,isAcct: false, img: person3},
 ];
